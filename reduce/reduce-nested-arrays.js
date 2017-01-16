b443c9b64f3be72a797078e4c96de2c595ba7997
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-01-16 18:07:25 +0000

    Setting up Scripts for taskMaster App

--- reduce/reduce-nested-arrays.js
+++ reduce/reduce-nested-arrays.js
@@ -1,3 +1,8 @@
+//The three big gotchas with reduce are:
+//Forgetting to return
+//Forgetting an initial value
+//Expecting an array when reduce returns a single value
+
 var array_of_arrays = [[1, 2], [3, 4], [5, 6]];
 var concatenated = array_of_arrays.reduce( function (previous, current) {
         return previous.concat(current);
